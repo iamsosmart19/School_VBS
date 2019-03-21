d4b59e1ccbf1aed6b06871c8cfa8bef383c77b46
Michael Chen <45020248+fl4mx@users.noreply.github.com>
2019-03-22 10:09:22 +1100

    Add files via upload
    Redo, with No Real Root detection for Quadratics.

---
+++ Michael/calculator/calculator/calculator/Form1.Designer.vb
@@ -0,0 +1,367 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.firstLabel = New System.Windows.Forms.Label()
+        Me.secondLabel = New System.Windows.Forms.Label()
+        Me.userInA = New System.Windows.Forms.TextBox()
+        Me.allClearButton = New System.Windows.Forms.Button()
+        Me.label0 = New System.Windows.Forms.Label()
+        Me.userInB = New System.Windows.Forms.TextBox()
+        Me.userInC = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.drawButton = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.userInE = New System.Windows.Forms.TextBox()
+        Me.userInD = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.quadraticDetectionLabel = New System.Windows.Forms.Label()
+        Me.root1Label = New System.Windows.Forms.Label()
+        Me.root2Label = New System.Windows.Forms.Label()
+        Me.clearButton = New System.Windows.Forms.Button()
+        Me.displayALabel = New System.Windows.Forms.Label()
+        Me.displayBLabel = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.displayCLabel = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.displayDLabel = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.displayELabel = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'firstLabel
+        '
+        Me.firstLabel.AutoSize = True
+        Me.firstLabel.Location = New System.Drawing.Point(60, 39)
+        Me.firstLabel.Name = "firstLabel"
+        Me.firstLabel.Size = New System.Drawing.Size(16, 13)
+        Me.firstLabel.TabIndex = 0
+        Me.firstLabel.Text = "a:"
+        '
+        'secondLabel
+        '
+        Me.secondLabel.AutoSize = True
+        Me.secondLabel.Location = New System.Drawing.Point(132, 39)
+        Me.secondLabel.Name = "secondLabel"
+        Me.secondLabel.Size = New System.Drawing.Size(16, 13)
+        Me.secondLabel.TabIndex = 1
+        Me.secondLabel.Text = "b:"
+        '
+        'userInA
+        '
+        Me.userInA.Location = New System.Drawing.Point(63, 55)
+        Me.userInA.Name = "userInA"
+        Me.userInA.Size = New System.Drawing.Size(66, 20)
+        Me.userInA.TabIndex = 2
+        '
+        'allClearButton
+        '
+        Me.allClearButton.Location = New System.Drawing.Point(63, 168)
+        Me.allClearButton.Name = "allClearButton"
+        Me.allClearButton.Size = New System.Drawing.Size(75, 23)
+        Me.allClearButton.TabIndex = 4
+        Me.allClearButton.TabStop = False
+        Me.allClearButton.Text = "All Clear (&q)"
+        Me.allClearButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.allClearButton.UseVisualStyleBackColor = True
+        '
+        'label0
+        '
+        Me.label0.AutoSize = True
+        Me.label0.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.label0.Location = New System.Drawing.Point(66, 9)
+        Me.label0.Name = "label0"
+        Me.label0.Size = New System.Drawing.Size(74, 29)
+        Me.label0.TabIndex = 12
+        Me.label0.Text = "x^4 + "
+        Me.label0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'userInB
+        '
+        Me.userInB.Location = New System.Drawing.Point(135, 55)
+        Me.userInB.Name = "userInB"
+        Me.userInB.Size = New System.Drawing.Size(66, 20)
+        Me.userInB.TabIndex = 13
+        '
+        'userInC
+        '
+        Me.userInC.Location = New System.Drawing.Point(207, 55)
+        Me.userInC.Name = "userInC"
+        Me.userInC.Size = New System.Drawing.Size(66, 20)
+        Me.userInC.TabIndex = 14
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(203, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(16, 13)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "c:"
+        '
+        'drawButton
+        '
+        Me.drawButton.Location = New System.Drawing.Point(63, 226)
+        Me.drawButton.Name = "drawButton"
+        Me.drawButton.Size = New System.Drawing.Size(75, 23)
+        Me.drawButton.TabIndex = 5
+        Me.drawButton.TabStop = False
+        Me.drawButton.Text = "Draw (&z)"
+        Me.drawButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.drawButton.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(348, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(16, 13)
+        Me.Label2.TabIndex = 19
+        Me.Label2.Text = "e:"
+        '
+        'userInE
+        '
+        Me.userInE.Location = New System.Drawing.Point(352, 55)
+        Me.userInE.Name = "userInE"
+        Me.userInE.Size = New System.Drawing.Size(66, 20)
+        Me.userInE.TabIndex = 18
+        '
+        'userInD
+        '
+        Me.userInD.Location = New System.Drawing.Point(279, 55)
+        Me.userInD.Name = "userInD"
+        Me.userInD.Size = New System.Drawing.Size(66, 20)
+        Me.userInD.TabIndex = 17
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(276, 39)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(16, 13)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "d:"
+        '
+        'quadraticDetectionLabel
+        '
+        Me.quadraticDetectionLabel.AutoSize = True
+        Me.quadraticDetectionLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.quadraticDetectionLabel.Location = New System.Drawing.Point(41, 257)
+        Me.quadraticDetectionLabel.Name = "quadraticDetectionLabel"
+        Me.quadraticDetectionLabel.Size = New System.Drawing.Size(0, 17)
+        Me.quadraticDetectionLabel.TabIndex = 20
+        '
+        'root1Label
+        '
+        Me.root1Label.AutoSize = True
+        Me.root1Label.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.root1Label.Location = New System.Drawing.Point(66, 286)
+        Me.root1Label.Name = "root1Label"
+        Me.root1Label.Size = New System.Drawing.Size(0, 15)
+        Me.root1Label.TabIndex = 21
+        '
+        'root2Label
+        '
+        Me.root2Label.AutoSize = True
+        Me.root2Label.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.root2Label.Location = New System.Drawing.Point(66, 308)
+        Me.root2Label.Name = "root2Label"
+        Me.root2Label.Size = New System.Drawing.Size(0, 15)
+        Me.root2Label.TabIndex = 22
+        '
+        'clearButton
+        '
+        Me.clearButton.Location = New System.Drawing.Point(63, 197)
+        Me.clearButton.Name = "clearButton"
+        Me.clearButton.Size = New System.Drawing.Size(75, 23)
+        Me.clearButton.TabIndex = 23
+        Me.clearButton.TabStop = False
+        Me.clearButton.Text = "Clear (&a)"
+        Me.clearButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.clearButton.UseVisualStyleBackColor = True
+        '
+        'displayALabel
+        '
+        Me.displayALabel.AutoSize = True
+        Me.displayALabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.displayALabel.Location = New System.Drawing.Point(40, 14)
+        Me.displayALabel.Name = "displayALabel"
+        Me.displayALabel.Size = New System.Drawing.Size(20, 24)
+        Me.displayALabel.TabIndex = 28
+        Me.displayALabel.Text = "a"
+        Me.displayALabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'displayBLabel
+        '
+        Me.displayBLabel.AutoSize = True
+        Me.displayBLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.displayBLabel.Location = New System.Drawing.Point(138, 15)
+        Me.displayBLabel.Name = "displayBLabel"
+        Me.displayBLabel.Size = New System.Drawing.Size(21, 24)
+        Me.displayBLabel.TabIndex = 30
+        Me.displayBLabel.Text = "b"
+        Me.displayBLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.Label5.Location = New System.Drawing.Point(165, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(74, 29)
+        Me.Label5.TabIndex = 29
+        Me.Label5.Text = "x^3 + "
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'displayCLabel
+        '
+        Me.displayCLabel.AutoSize = True
+        Me.displayCLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.displayCLabel.Location = New System.Drawing.Point(235, 14)
+        Me.displayCLabel.Name = "displayCLabel"
+        Me.displayCLabel.Size = New System.Drawing.Size(20, 24)
+        Me.displayCLabel.TabIndex = 32
+        Me.displayCLabel.Text = "c"
+        Me.displayCLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.Label7.Location = New System.Drawing.Point(261, 9)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(74, 29)
+        Me.Label7.TabIndex = 31
+        Me.Label7.Text = "x^2 + "
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'displayDLabel
+        '
+        Me.displayDLabel.AutoSize = True
+        Me.displayDLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.displayDLabel.Location = New System.Drawing.Point(341, 13)
+        Me.displayDLabel.Name = "displayDLabel"
+        Me.displayDLabel.Size = New System.Drawing.Size(21, 24)
+        Me.displayDLabel.TabIndex = 34
+        Me.displayDLabel.Text = "d"
+        Me.displayDLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.Label10.Location = New System.Drawing.Point(368, 10)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(50, 29)
+        Me.Label10.TabIndex = 33
+        Me.Label10.Text = "x + "
+        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'displayELabel
+        '
+        Me.displayELabel.AutoSize = True
+        Me.displayELabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.displayELabel.Location = New System.Drawing.Point(424, 13)
+        Me.displayELabel.Name = "displayELabel"
+        Me.displayELabel.Size = New System.Drawing.Size(21, 24)
+        Me.displayELabel.TabIndex = 36
+        Me.displayELabel.Text = "e"
+        Me.displayELabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.Label12.Location = New System.Drawing.Point(461, 10)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(52, 29)
+        Me.Label12.TabIndex = 35
+        Me.Label12.Text = " = 0"
+        Me.Label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.displayELabel)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.displayDLabel)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.displayCLabel)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.displayBLabel)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.displayALabel)
+        Me.Controls.Add(Me.clearButton)
+        Me.Controls.Add(Me.root2Label)
+        Me.Controls.Add(Me.root1Label)
+        Me.Controls.Add(Me.quadraticDetectionLabel)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.userInE)
+        Me.Controls.Add(Me.userInD)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.userInC)
+        Me.Controls.Add(Me.userInB)
+        Me.Controls.Add(Me.label0)
+        Me.Controls.Add(Me.drawButton)
+        Me.Controls.Add(Me.allClearButton)
+        Me.Controls.Add(Me.userInA)
+        Me.Controls.Add(Me.secondLabel)
+        Me.Controls.Add(Me.firstLabel)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents firstLabel As Label
+    Friend WithEvents secondLabel As Label
+    Friend WithEvents userInA As TextBox
+    Friend WithEvents allClearButton As Button
+    Friend WithEvents label0 As Label
+    Friend WithEvents userInB As TextBox
+    Friend WithEvents userInC As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents drawButton As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents userInE As TextBox
+    Friend WithEvents userInD As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents quadraticDetectionLabel As Label
+    Friend WithEvents root1Label As Label
+    Friend WithEvents root2Label As Label
+    Friend WithEvents clearButton As Button
+    Friend WithEvents displayALabel As Label
+    Friend WithEvents displayBLabel As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents displayCLabel As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents displayDLabel As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents displayELabel As Label
+    Friend WithEvents Label12 As Label
+End Class
